1706ad8fe51fa8b2523f82b720d263774015d3c7
YENUMULA AJAY KUMAR REDDY <113223205+star1233914@users.noreply.github.com>
2023-01-10 14:18:17 +0530

    Create marks.java

---
+++ marks.java
@@ -0,0 +1,20 @@
+import java.Scanner;
+class marks
+{
+public static void main(String[]arg)
+int a,b,c;
+Scanner SC=new Scanner(System.in);
+System.out.println("maths :");
+a=SC.nextInt();
+System.out.println("Science :");
+
+b=SC.nextInt();
+System.out.println("social :");
+c=SC.nextInt();
+{
+if(a+b+c/3=>75);
+System.out.println("the acquried grade is S");
+else
+System.out.println("the aquried grade is A");
+}
+}
