1dd2567a3c89b10618e3a0909fc59451d9e3bfe4
YENUMULA AJAY KUMAR REDDY <113223205+star1233914@users.noreply.github.com>
2023-01-10 14:17:03 +0530

    Create string_length.java

---
+++ string_length.java
@@ -0,0 +1,10 @@
+class string_length
+{
+public static void main(String[]args)
+{
+String str="river";
+int length=str.length();
+System.out.println("the length of the given string is:" +length);
+}
+}
+
