d3ad434c932d061e4fc8d845d571acca30e4b9cd
YENUMULA AJAY KUMAR REDDY <113223205+star1233914@users.noreply.github.com>
2023-01-10 14:14:20 +0530

    Create gfg.java

---
+++ gfg.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+class GFG 
+{
+public static void main (String[] args) 
+{
+String str= "Ajay", nstr="";
+char ch;
+System.out.print("Original word: ");
+System.out.println("Ajay"); 
+for (int i=0; i<str.length(); i++)
+{
+ch= str.charAt(i); 
+nstr= ch+nstr; 
+System.out.println("Reversed word: "+ nstr);
+}
+}
+}
